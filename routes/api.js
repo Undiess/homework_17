6909e0be782fac6eca56f98e2b5c668aee0070a0
Undiess <nguyen.andy67@gmail.com>
2020-07-10 02:14:21 +0930

    added put api

--- routes/api.js
+++ routes/api.js
@@ -11,4 +11,20 @@ module.exports = function(app){
     });
 
 
+    app.post("/api/workouts",({ body }, res) =>{ 
+        db.Workout.create(body)
+        .then(db => {
+            res.json(db);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+    })
+
+    app.put("/api/workouts/:id" ,({body, params}, res) => {
+        const id = params.id;
+        console.log(body)
+
+    })
+
 }
